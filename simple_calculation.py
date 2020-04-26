18148d782e04372a8ed71f4312123edc545faac6
Bilal Ahmad <mebilalahmad1@gmail.com>
2020-04-26 18:05:35 +0500

    FirstAssignment

---
+++ simple_calculation.py
@@ -0,0 +1,15 @@
+num1=input("Enter First Number:")
+num2=input("Enter Seconc Number:")
+addition=int(num1)+int(num2)
+subtraction=int(num1)-int(num2)
+multiplication=int(num1)*int(num2)
+division=int(num1)/int(num2)
+print("Addition:",addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
+
+
+
+
+
